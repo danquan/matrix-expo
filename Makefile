0c9ad5c304bff81ef8f57bde438077ee8b3bc76a
danquan <dangquanattvhouse@gmail.com>
2024-09-12 10:50:52 +0700

    add openmp code

--- Makefile
+++ Makefile
@@ -1,3 +1,10 @@
 all:
-	g++ -O2 src/matrix_expo.cpp -o matrix_expo
-	g++ -lpthread -O2 src/pthreads_1.1.cpp -o matrix_expo_pthreads
+	# g++ -O2 src/matrix_expo.cpp -o matrix_expo
+	# g++ -lpthread -O2 src/pthreads_1.1.cpp -o matrix_expo_pthreads_1.1
+	# g++ -lpthread -O2 src/pthreads_1.cpp -o matrix_expo_pthreads_1
+	# g++ -fopenmp -O2 src/openmp_1.cpp -o matrix_expo_openmp_1
+	# g++ -fopenmp -O2 src/openmp_1.1.cpp -o matrix_expo_openmp_1.1
+	g++ -fopenmp -O2 src/openmp_2.cpp -o matrix_expo_openmp_2
+	# g++ -fopenmp -O2 src/openmp_2.1.cpp -o matrix_expo_openmp_2.1
+	# g++ -fopenmp -O2 src/openmp_2.1.cpp -o matrix_expo_openmp_2.2
+
--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
-./matrix_expo --input1 test/1k1k1e9.inp --input2 test/1k1k1e9.inp --output test/1k1k1e9-true.out
-./matrix_expo_pthreads rix_Expo -nt 5 --input1 test/1k1k1e9.inp --input2 test/1k1k1e9.inp --output test/1k1k1e9.out
+# ./matrix_expo --input1 test/101010.inp --input2 test/101010.inp --output output/101010-true.out
+# ./matrix_expo_pthreads_1.1 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_pthreads_1.1_2.out
+# ./matrix_expo_pthreads_1 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_pthreads_1_2.out
+# ./matrix_expo_openmp_1 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_openmp_1_2.out
+# ./matrix_expo_openmp_1.1 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_openmp_1.1_2.out
+./matrix_expo_openmp_2 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_openmp_2_2.out
+# ./matrix_expo_openmp_2.1 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_openmp_2.1_2.out
+# ./matrix_expo_openmp_2.2 -nt 2 --input1 test/101010.inp --input2 test/101010.inp --output output/101010_openmp_2.2_2.out
+
